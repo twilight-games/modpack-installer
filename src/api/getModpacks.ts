7b9e22c69604c720285cfbd823039c3004db56d0
Lucas van Beek <info@lucasvanbeek.nl>
2022-01-06 17:30:03 +0100

    Replace API promise with reference, add error handling & loading animation

--- src/api/getModpacks.ts
+++ src/api/getModpacks.ts
@@ -1,51 +1,72 @@
-import {fetch, Response, ResponseType} from '@tauri-apps/api/http'
+import { fetch, Response, ResponseType } from "@tauri-apps/api/http";
+import { reactive, Ref, ref, toRefs } from "vue";
 
 export interface Mod {
-    id: string;
-    name: string;
-    filename: string;
-    state: string;
-    url: string;
-    hash: string;
+  id: string;
+  name: string;
+  filename: string;
+  state: string;
+  url: string;
+  hash: string;
 }
 
 export interface Modpack {
-    id: string;
-    name: string;
-    description: string;
-    minecraftVersion: string;
-    loaderVersion: string;
-    mods: Mod[];
+  id: string;
+  name: string;
+  description: string;
+  minecraftVersion: string;
+  loaderVersion: string;
+  mods: Mod[];
 }
 
 export interface ModpackRequest {
-    modpacks: [];
+  modpacks: [];
 }
 
 export const rawDataToModpackObject = (data: any): Modpack => ({
-    id: data.id,
-    name: data.name,
-    description: data.description,
-    minecraftVersion: data.minecraft_version,
-    loaderVersion: data.loader_version,
-    mods: (data.mods || []).map(rawDataToModObject),
+  id: data.id,
+  name: data.name,
+  description: data.description,
+  minecraftVersion: data.minecraft_version,
+  loaderVersion: data.loader_version,
+  mods: (data.mods || []).map(rawDataToModObject),
 });
 
 export const rawDataToModObject = (data: any): Mod => ({
-    id: data.id,
-    name: data.name,
-    filename: data.filename,
-    state: data.state,
-    url: data.url,
-    hash: data.hash,
+  id: data.id,
+  name: data.name,
+  filename: data.filename,
+  state: data.state,
+  url: data.url,
+  hash: data.hash,
 });
 
-export default (): Promise<Modpack[]> => {
-    return new Promise((resolve, reject) => {
-        fetch<any>('https://modpack.vloedje.nl/test.json')
-        .then(({ data }) => resolve(
-            data.modpacks.map(rawDataToModpackObject),
-        ))
-        .catch(reject);
+export default (): {
+  result: Ref<Modpack[]>;
+  error: Ref<any>;
+  isLoading: Ref<boolean>;
+} => {
+  const state = reactive({
+    isLoading: false,
+    error: null as any,
+  });
+
+  const result = ref<Modpack[]>([]);
+  state.isLoading = true;
+  fetch<any>("https://modpack.vloedje.nl/test.json")
+    .then(({ data }) => {
+      result.value = data.modpacks.map(rawDataToModpackObject);
+      console.log(result.value);
+    })
+    .catch((error) => {
+      state.error = error;
+    })
+    .finally(() => {
+      state.isLoading = false;
     });
+
+  return {
+    result,
+    ...toRefs(state),
+  };
 };
---
+++ src/components/AlertBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="rounded-md bg-red-800 p-4">
+        <div class="flex">
+            <div class="flex-shrink-0">
+                <XCircleIcon class="h-5 w-5 text-red-200" aria-hidden="true" />
+            </div>
+            <div class="ml-3">
+                <h3 class="text-sm font-medium text-red-100">An error occured:</h3>
+                <p class="mt-2 text-sm text-white" v-text="error" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { XCircleIcon } from '@heroicons/vue/solid';
+
+const props = defineProps<{
+    error: string;
+}>()
+</script>
